fb11f65ff013597dc56278e837fc4d8420632718
Josephg17 <gjelajjoseph313@gmail.com>
2024-06-09 06:35:37 -0400

    project 1 and 2 finished

--- chapter 2 projects joseph/js02/js02/project02/project02-02_txt.js
+++ chapter 2 projects joseph/js02/js02/project02/project02-02_txt.js
@@ -3,9 +3,25 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Joseph Gjelaj
+      Date:   6/9/24
 
       Filename: project02-02.js
  */
- 
+// you have to put it in a browser for it to show
+
+function verifyForm() {
+      var name = document.getElementById("name").value;
+      var email = document.getElementById("email").value;
+      var phone = document.getElementById("phone").value;
+  
+      var isValid = name && email && phone;
+      if (isValid) {
+          window.alert("Thank you!");
+      } else {
+          window.alert("Please fill in all fields");
+      }
+  }
+  
+  document.getElementById("submit").addEventListener("click", verifyForm);
+  
